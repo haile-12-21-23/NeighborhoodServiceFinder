885bcd48ae2e537387479e113c70b1a5af55596c
Hailemariam addisu <105885701+haile-12-21-23@users.noreply.github.com>
2025-10-16 17:10:51 +0300

    Generate token util defined

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import morgan from 'morgan';
 import connectDB from './config/db.js';
 // import userRoutes from './routes/userRoutes.js';
 // import serviceRoutes from './routes/serviceRoutes.js';
-// import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
@@ -22,8 +22,8 @@ app.use(morgan('dev'));
 // app.use('/api/services', serviceRoutes);
 
 // Error handling middleware
-// app.use(notFound);
-// app.use(errorHandler);
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
